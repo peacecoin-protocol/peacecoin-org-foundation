005e65566f3501293c6c9072c3dfc050d1c481dd
Naoki MURATA <murata@b42.co.jp>
2025-04-03 16:19:51 +0900

    fix: Delete information that is not ready for launch

--- app/constants/crowdin.ts
+++ app/constants/crowdin.ts
@@ -1,5 +1,4 @@
 export const translatedProgress = [
   { locale: 'en', progress: 100, total: 304 },
-  { locale: 'ja-JP', progress: 4, total: 304 },
-  { locale: 'pt-PT', progress: 3, total: 304 },
+  { locale: 'ja-JP', progress: 100, total: 304 },
 ]
